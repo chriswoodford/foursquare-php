ad61e1a00527addf24d7a51ede1441f52e63a779
Chris Woodford <chris.woodford@gmail.com>
2012-11-07 02:01:00 -0500

    started implementing venues functionality

--- lib/TheTwelve/Foursquare/VenuesGateway.php
+++ lib/TheTwelve/Foursquare/VenuesGateway.php
@@ -5,4 +5,41 @@ namespace TheTwelve\Foursquare;
 class VenuesGateway extends EndpointGateway
 {
 
+    /**
+     * Enter description here ...
+     * @param string $venueId
+     * @return stdClass
+     */
+    public function getVenue($venueId)
+    {
+
+        $resource = '/venues/' . $venueId;
+        $response = $this->makeApiRequest($resource);
+
+        return $response->venue;
+
+    }
+
+    public function add()
+    {}
+
+    /**
+     * Returns a hierarchical list of categories applied to venues.
+     * When designing client applications, please download this list
+     * only once per session, but also avoid caching this data for
+     * longer than a week to avoid stale information.
+     * @return array
+     */
+    public function categories()
+    {
+
+        $resource = '/venues/categories';
+        $response = $this->makeApiRequest($resource);
+
+        return $response->categories;
+
+    }
+
+
+
 }
